fb48d6ee2f0ca31ffaa40589a07365727f128d76
Marilena Sales <marilena.sales@gmail.com>
2022-01-22 22:02:34 -0300

    Create Tmdb.js

---
+++ src/Tmdb.js
@@ -0,0 +1,13 @@
+const API_KEY ='3db42a173ff5cf5d10d55cef8838ca75';
+const API_BASE='https://api.themoviedb.org/3';
+
+/*
+- originais da netflix
+- recomentdados(trending)
+- em alta(top rated)
+- ação
+- comédia
+- terror
+- romance
+- documentários
+*/
