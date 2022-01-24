c0b4c5e96b37a04a6eff1769ce8f9c672ced1d0d
Marilena Sales <marilena.sales@gmail.com>
2022-01-24 20:48:50 -0300

    featuredmovie

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, {useEffect, useState} from "react";
 import Tmdb from './Tmdb';
 import './App.css'
 import MovieRow from "./components/MovieRow/MovieRow";
+import FeaturedMovie from './components/FeaturedMovie/FeaturedMovie';
 
 
 export default() => {
   //para ser exibida a lista
   const [movieList, setMovieList] = useState([]);
+  //para o destaque ser exibido
+  const[featuredData, setFeaturedData] = useState(null);
 
   useEffect(() =>{
   //executa esse bloco ao carregar
@@ -15,6 +18,14 @@ export default() => {
     let list = await Tmdb.getHomeList();
     //console.log(list);
     setMovieList(list);
+
+    //mostrando o filme em destaque (featured)
+    let originals = list.filter(i=>i.slug === 'originals');//pegar o item do array originals
+    let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+    let chosen = originals[0].items.results[randomChosen];
+    let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+    setFeaturedData(chosenInfo);
+    
   }
 
   loadAll();
@@ -22,6 +33,11 @@ export default() => {
 
   return(
     <div className="page">
+
+    {featuredData &&
+      <FeaturedMovie item={featuredData} />
+    }
+
       <section className="lists">
         {movieList.map((item, key) =>(
           <MovieRow key={key} title={item.title} items={item.items}/>
