e0e00c64f1f267cb83234363c2f6279cc16cd2c6
Marilena Sales <marilena.sales@gmail.com>
2022-01-23 22:11:44 -0300

    Update Tmdb.js

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -12,13 +12,56 @@ const API_BASE='https://api.themoviedb.org/3';
 - documentários
 */
 
+const basicFetch = async (endpoint) => {
+  const req = await fetch(`${API_BASE}${endpoint}`);
+  const json = await req.json();
+  return json;
+}
 
-export default(){
+//lista que será exibida montando url
+export default{
   getHomeList: async () => {
     return [
       {
-        
-      }
+        slug: 'originals',
+        title: 'Originais do Netflix',
+        items: await basicFetch(`/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`)
+      },
+      {
+        slug: 'trending',
+        title: 'Recomendados para Você',
+        items: []
+      },
+      {
+        slug: 'toprated',
+        title: 'Em Alta',
+        items: []
+      },
+      {
+        slug: 'action',
+        title: 'Ação',
+        items: []
+      },
+      {
+        slug: 'comedy',
+        title: 'Comédia',
+        items: []
+      },
+      {
+        slug: 'horror',
+        title: 'Terror',
+        items: []
+      },
+      {
+        slug: 'romance',
+        title: 'Romance',
+        items: []
+      },
+      {
+        slug: 'documentary',
+        title: 'Documentários',
+        items: []
+      },
     ]
   }
 }
